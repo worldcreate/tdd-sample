68a97522787a21edf4082d183aba3569147eefa7
worldcreate <furatsuki_fiskm@yahoo.co.jp>
2018-05-13 00:34:24 +0900

    red状態のinsertTenCoinTestテストを追加

---
+++ src/test/java/work/snowglobe/tdd/sample/Step0Test.java
@@ -0,0 +1,32 @@
+package work.snowglobe.tdd.sample;
+
+import org.junit.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.UnsupportedEncodingException;
+
+import static org.junit.Assert.*;
+
+
+/**
+ * <ul>
+ *     <li>10円玉、50円玉、100円玉、500円玉、1000円札を１つずつ投入できる。</li>
+ *     <li>投入は複数回できる。</li>
+ *     <li>投入金額の総計を取得できる。</li>
+ *     <li>払い戻し操作を行うと、投入金額の総計を釣り銭として出力する。</li>
+ * </ul>
+ *
+ */
+public class Step0Test {
+
+    /**
+     * 10円挿入テスト
+     *
+     * @throws UnsupportedEncodingException
+     */
+    @Test public void insertTenCoinTest() throws UnsupportedEncodingException {
+        String tenCoinInputString = "10";
+        VendingMachine vendingMachine = new VendingMachine(new ByteArrayInputStream(tenCoinInputString.getBytes("utf-8")));
+        vendingMachine.insertCoin();
+    }
+}
